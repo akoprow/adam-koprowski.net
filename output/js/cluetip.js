bc043a8acf35b9f6c824f8b9157684918b408bfa
Adam Koprowski <Adam.Koprowski@mlstate.com>
2010-06-15 22:33:16 +0200

    Few style fixes;
    mainly to bring back plan, transparent tips for books' authors.

--- output/js/cluetip.js
+++ output/js/cluetip.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
 	$('span.author').cluetip({
-		showTitle: false, cluetipClass: 'rounded', width: '630px', attribute: 'rel', 
+		showTitle: false, cluetipClass: 'author', width: '630px', attribute: 'rel', 
 		 arrows: false, cursor: 'crosshair', hoverClass: 'highlight' });
 
 	$('img.book-comment').cluetip({
 		splitTitle: '|', arrows: true, local: true, hideLocal: true, cluetipClass: 'jtip',
-		dropShadow: false});
+		dropShadow: false, cursor: 'crosshair', tracking: true});
 
 });
